dcd96d0074ba5cd6feb8a68d3906f86e80d33a83
lanxfikayo <fikayobasit@gmail.com>
2026-06-06 12:21:49 +0100

    Add save_report function

--- combined_project/main.py
+++ combined_project/main.py
@@ -179,6 +179,17 @@ def profile_transform(enriched_records,logger):
 
     return transformed
 
+def save_report(df,config,logger):
+    try:
+        df.to_csv(config.output_file,index=False)
+        df_rows = len(df)
+        logger.info(f"{df_rows} were written to {config.output_file}")
+    except OSError as e:
+        raise PipelineError(f"Writing of the dataframe to {config.output_file} failed: {e}")
+
+
+
+
 
 
 
